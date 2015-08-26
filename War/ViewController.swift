9ebbfb386be0084b59b1945b7c62a894f61ccdae
senator.temp@gmail.com <timurusmanov@MacBook-Air-Timur.local>
2015-08-26 13:25:43 +0600

    Initial Commit

---
+++ War/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Timur Usmanov on 26.08.15.
+//  Copyright (c) 2015 Timur Usmanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
